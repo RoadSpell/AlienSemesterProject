415ea15279940a4dc8f23c9d8d30e120e3b19156
Mert YILMAZ <mertyilmaz.dev@gmail.com>
2025-01-18 19:31:14 +0100

    Poop Animation

--- Assets/Scripts/Poop.cs
+++ Assets/Scripts/Poop.cs
@@ -5,6 +5,15 @@ public class Poop : MonoBehaviour
 {
     public ItemType itemType = ItemType.Carbon;
     private float _sinDegree = 0f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 spawnInitialForce;
+    [SerializeField] private Vector3 spawnInitialTorque;
+
+    private void Start()
+    {
+        rb.AddForce(spawnInitialForce, ForceMode.Impulse);
+        rb.angularVelocity = spawnInitialTorque;
+    }
 
     private void Update()
     {
@@ -14,7 +23,8 @@ public class Poop : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 oldPos = transform.position;
-        transform.position = new Vector3(oldPos.x, (Mathf.Sin(_sinDegree) / 4) + 0.5f, oldPos.z);
+        rb.MovePosition(new Vector3(oldPos.x, (Mathf.Sin(_sinDegree) / 8) + 0.5f, oldPos.z));
+        //transform.position = new Vector3(oldPos.x, (Mathf.Sin(_sinDegree) / 4) + 0.5f, oldPos.z);
     }
 
     private void OnTriggerEnter(Collider other)
